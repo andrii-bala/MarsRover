bfdc78ec0e49722d5a5e3cde233cf442ccdd414f
Andrii Bala <seQVAER@MacBook-Pro-Andrii.local>
2018-12-24 11:30:19 +0200

    add project

---
+++ Bala.Andrii.MarsRover/Bala.Andrii.MarsRover/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bala.Andrii.MarsRover
+//
+//  Created by Andrii Bala on 12/24/18.
+//  Copyright © 2018 iThinkers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bala.Andrii.MarsRover/Bala.Andrii.MarsRover/Support/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Bala.Andrii.MarsRover
+//
+//  Created by Andrii Bala on 12/24/18.
+//  Copyright © 2018 iThinkers. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
